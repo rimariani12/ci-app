c342b9098ec8527a47b0acf20ffc46f13f76f001
Rima Riani <rima_173040117@mail.unpas.ac.id>
2019-10-18 17:50:40 +0700

    membuat form tambah data mahasiswa

---
+++ application/views/mahasiswa/index.php
@@ -0,0 +1,18 @@
+<div class="container">
+
+	<div class="row mt-3">
+		<div class="col-md-6">
+			<a href="" class="btn btn-primary">Tambah Data Mahasiswa</a>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-3">
+			<h3>Daftar Mahasiswa</h3>
+			<ul class="list-group">
+				<?php foreach($mahasiswa as $mhs) : ?>
+ 			 <li class="list-group-item"><?= $mhs['nama']; ?></li>
+  				<?php endforeach; ?>
+			</ul>
+		</div>
+	</div>
+</div>
---
+++ application/views/mahasiswa/tambah.php
@@ -0,0 +1,22 @@
+<div class="container">
+	<div class="row mt-3">
+		<div class="col-md-6">
+			<div class="card">
+				<div class="card">
+  					<div class="card-header">
+ 					   Form Tambah Data Mahasiswa
+					</div>
+  					<div class="card-body">
+   						<form action="" method="post">
+						<div class="form-group">
+   					 	<label for="nama">Nama</label>
+   					 	<input type="text" class="form-control" id="nama">
+  				</div>
+				</form>
+  				</div>
+			</div>
+			
+		</div>
+	</div>
+	</div>
+</div>
