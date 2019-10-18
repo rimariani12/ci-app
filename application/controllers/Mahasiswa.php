d04034f7e67bc6dd6a47c99ae33ddb45b7e14a65
Rima Riani <rima_173040117@mail.unpas.ac.id>
2019-10-18 22:12:25 +0700

    menambahkan ubah

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -49,4 +49,25 @@ class Mahasiswa extends CI_Controller{
 		$this->load->view('mahasiswa/detail', $data);
 		$this->load->view('templates/footer');
 	}
+
+	public function ubah($id){
+		$data['judul'] = 'Form Ubah Data Mahasiswa';
+		$data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
+		$data['jurusan'] = ['Teknik Informatika', 'Teknik Mesin', 'Teknik Planologi', 'Teknik Pangan', 'Teknik Industri', 'Teknik Lingkungan'];
+
+		$this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('nrp', 'NRP', 'required|numeric');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('templates/header', $data);
+			$this->load->view('mahasiswa/ubah', $data);
+			$this->load->view('templates/footer');
+		}else{
+			$this->Mahasiswa_model->ubahDataMahasiswa();
+			$this->session->set_flashdata('flash', 'Diubah');
+			redirect('mahasiswa');
+		}
+		
+	}
 }
--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -25,4 +25,16 @@ class Mahasiswa_model extends CI_model{
 	public function getMahasiswaById($id){
 		return $this->db->get_where('mahasiswa', ['id' => $id])->row_array();
 	}
+
+	public function ubahDataMahasiswa(){
+		$data = [
+			"nama" => $this->input->post('nama', true),
+			"nrp" => $this->input->post('nrp', true),
+			"email" => $this->input->post('email', true),
+			"jurusan" => $this->input->post('jurusan', true)
+		];
+
+		$this->db->where('id', $this->input->post('id'));
+		$this->db->update('mahasiswa', $data);
+	}
 }
