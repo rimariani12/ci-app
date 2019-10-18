4d43167034b73e92b507d590b7ff0c4a0db50de9
Rima Riani <rima_173040117@mail.unpas.ac.id>
2019-10-18 20:17:15 +0700

    menambahkan hapus

--- application/views/mahasiswa/tambah.php
+++ application/views/mahasiswa/tambah.php
@@ -7,16 +7,39 @@
  					   Form Tambah Data Mahasiswa
 					</div>
   					<div class="card-body">
+  						<?php if (validation_errors() ) : ?>
+  						<div class="alert alert-danger" role="alert">
+ 						 <?= validation_errors();  ?>
+						</div>
+  						<?php endif; ?>
    						<form action="" method="post">
-						<div class="form-group">
+					<div class="form-group">
    					 	<label for="nama">Nama</label>
-   					 	<input type="text" class="form-control" id="nama">
+   					 	<input type="text" name="nama" class="form-control" id="nama">
   				</div>
-				</form>
+  				<div class="form-group">
+   					 	<label for="nrp">NRP</label>
+   					 	<input type="text" name="nrp" class="form-control" id="nrp">
+				
   				</div>
-			</div>
-			
+  				<div class="form-group">
+   					 	<label for="email">email</label>
+   					 	<input type="text" name="email" class="form-control" id="email">
+				</div>
+				<div class="form-group">
+    			<label for="jurusan">Jurusan</label>
+   				<select multiple class="form-control" id="jurusan" name="jurusan">
+      			<option value="Teknik Informatika">Teknik Informatika</option>
+      			<option value="Teknik Industri">Teknik Industri</option>
+     			<option value="Teknik Pangan">Teknik Pangan</option>
+      			<option value="Teknik Mesin">Teknik Mesin</option>
+      			<option value="Teknik Planologi">Teknik Planologi</option>
+      			<option value="Teknik Lingkungan">Teknik Lingkungan</option>
+    			</select>
+ 				</div>
+ 				<button type="submit" name="tambah" class="btn btn-primary float-right">Tambah Data</button>
+ 				</form>
 		</div>
-	</div>
+
 	</div>
 </div>
