63d18605f68ad15dbbbca825691ff859fccad298
Rima Riani <rima_173040117@mail.unpas.ac.id>
2019-10-19 09:44:51 +0700

    menambahkan searching pagination

--- application/views/peoples/index.php
+++ application/views/peoples/index.php
@@ -1,8 +1,22 @@
 <div class="container">
+		<h3 class="mt-3">List of Peoples</h3>
+
+		<div class="row">
+			<div class="col-md-5">
+				<form action="<?= base_url('peoples'); ?>" method="post">
+					<div class="input-group mb-3">
+  					<input type="text" class="form-control" placeholder="Search keyword.." name="keyword" autocomplete="off" autofocus>
+  					<div class="input-group-append">
+  						<input class="btn btn-primary" type="submit" name="submit">
+  					</div>
+					</div>
+				</form>
+			</div>
+		</div>
+
 	<div class="row">
 		<div class="col-md-6">
-			<h3 class="mt-3">List of Peoples</h3>
-
+			<h5>Result : <?= $total_rows; ?></h5>
 			<table class=" table">
 				<thead>
 					<tr>
@@ -13,6 +27,13 @@
 					</tr>
 				</thead>
 				<tbody>
+					<?php if(empty($peoples)) : ?>
+						<tr>
+							<td colspan="4">
+								<div class="alert alert-danger" role="alert">Data Not Found!</div>
+							</td>
+						</tr>
+					<?php endif; ?>
 					<?php foreach ($peoples as $people) : ?>
 					<tr>
 						<th><?= ++$start; ?></th>
